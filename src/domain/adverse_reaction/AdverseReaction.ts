5a52fc1402a218c7931f50ba53b151402de4e75a
AnibalGonzalezV <165087800+AnibalGonzalezV@users.noreply.github.com>
2024-10-01 01:00:01 -0300

    implementacion clase AdverseReaction

---
+++ src/domain/adverse_reaction/AdverseReaction.ts
@@ -0,0 +1,14 @@
+import Drug from "../drug/Drug";
+export default class AdverseReaction{
+    constructor(private drug1: Drug, private drug2: Drug){}
+
+    getDrug1(): Drug{
+        return this.drug1
+    }
+
+    getDrug2(): Drug{
+        return this.drug2
+    }
+
+    
+}
