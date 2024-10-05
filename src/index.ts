7c99575a2e222d13c0c0f3936c33a24d8e1d7551
DiegoWEBD <diegomz.webdev@gmail.com>
2024-10-05 16:11:21 -0300

    organizacion de capas

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import dotenv from 'dotenv'
 import Api from './presentation/Api'
-import User from './domain/user/User'
-import Teacher from './domain/teacher/Teacher'
-import DrugTypeRepository from './domain/drug_type/DrugTypeRepository'
 
 dotenv.config()
 
