db75d3a0fa8dc1d0e6eb7a02b07b67cf202e592b
Matias364 <130679251+Matias364@users.noreply.github.com>
2024-10-06 11:29:49 -0300

    Correccion finbyname

--- src/domain/drug/DrugRepository.ts
+++ src/domain/drug/DrugRepository.ts
@@ -3,7 +3,7 @@ import Drug from './Drug'
 export default interface DrugRepository {
 	add: (drug: Drug) => Promise<void>
 	getAll: () => Promise<Array<Drug>>
-	findByType: (name: string) => Promise<Drug | undefined>
+	findByName: (name: string) => Promise<Drug | undefined>
 	update: (name: string, newValues: Drug) => Promise<void>
 	delete: (drug: Drug) => Promise<void>
 }
