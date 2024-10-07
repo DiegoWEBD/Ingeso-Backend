3d375d2b8c8e64adc963993b31acd168969dd8b2
DiegoWEBD <diegomz.webdev@gmail.com>
2024-10-07 14:48:29 -0300

    cambios de formato

--- src/domain/drug_type/DrugTypeRepository.ts
+++ src/domain/drug_type/DrugTypeRepository.ts
@@ -2,7 +2,7 @@ import DrugType from './DrugType'
 
 export default interface DrugTypeRepository {
 	add: (drugType: DrugType) => Promise<void>
-	getAll: () => Promise<DrugType[]>
+	getAll: () => Promise<Array<DrugType>>
 	findByType: (dtype: string) => Promise<DrugType | undefined>
 	update: (dtype: string, newValues: DrugType) => Promise<void>
 	delete: (drugType: DrugType) => Promise<void>
