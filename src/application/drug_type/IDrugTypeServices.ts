5a7aa81e0caacbb411e7868e312a9730e5ee9345
DiegoWEBD <diegomz.webdev@gmail.com>
2024-10-07 20:21:02 -0300

    cambio en manejo de arreglos

--- src/application/drug_type/IDrugTypeServices.ts
+++ src/application/drug_type/IDrugTypeServices.ts
@@ -1,6 +1,6 @@
 import DrugType from '../../domain/drug_type/DrugType'
 
 export default interface IDrugTypeServices {
-	getDrugTypes: () => Promise<DrugType[]>
+	getDrugTypes: () => Promise<Array<DrugType>>
 	registerDrugType: (dtype: string, description: string) => Promise<DrugType>
 }
