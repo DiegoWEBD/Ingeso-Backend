f6e784209077ea3b714f0154267a85ac4501374d
DiegoWEBD <diegomz.webdev@gmail.com>
2024-10-08 17:56:21 -0300

    implementación clase DrugServices

---
+++ src/application/drug/DrugServices.ts
@@ -0,0 +1,38 @@
+import AdministrationRouteRepository from '../../domain/administration_route/AdministrationRouteRepository'
+import DrugRepository from '../../domain/drug/DrugRepository'
+import DrugTypeRepository from '../../domain/drug_type/DrugTypeRepository'
+import IDrugServices from './IDrugServices'
+import { makeAddAdverseReactionBetweenDrugs } from './use_cases/add_adverse_reaction_between_drugs'
+import { makeAddDrugAdministrationRoute } from './use_cases/add_drug_administration_route'
+import { makeAddDrugTypeToDrug } from './use_cases/add_drug_type_to_drug'
+import { makeGetDrugInformation } from './use_cases/get_drug_information'
+import { makeRegisterDrug } from './use_cases/register_drug'
+
+export default class DrugServices implements IDrugServices {
+	constructor(
+		private drugRepository: DrugRepository,
+		private drugTypeRepository: DrugTypeRepository,
+		private administrationRouteRepository: AdministrationRouteRepository
+	) {}
+
+	getDrugInformation = makeGetDrugInformation(this.drugRepository)
+	registerDrug = makeRegisterDrug(
+		this.drugRepository,
+		this.drugTypeRepository,
+		this.administrationRouteRepository
+	)
+	addDrugTypeToDrug = makeAddDrugTypeToDrug(
+		this.drugRepository,
+		this.drugTypeRepository
+	)
+	addAdverseReactionBetweenDrugs = makeAddAdverseReactionBetweenDrugs(
+		this.drugRepository
+	)
+	addDrugAdministrationRoute = makeAddDrugAdministrationRoute(
+		this.drugRepository,
+		this.administrationRouteRepository
+	)
+	getDrugsNames = (): Promise<string[]> => {
+		throw new Error('TO DO')
+	}
+}
