7e30d417587a68b5af28a2328502a6500356dcc2
DiegoWEBD <diegomz.webdev@gmail.com>
2024-10-08 16:53:19 -0300

    implementación caso de uso agregar tipo de fármaco a un fármaco

--- src/application/drug/use_cases/add_drug_type_to_drug.ts
+++ src/application/drug/use_cases/add_drug_type_to_drug.ts
@@ -1,1 +1,30 @@
-export const makeAddDrugTypeToDrug = () => {}
+import Drug from '../../../domain/drug/Drug'
+import DrugRepository from '../../../domain/drug/DrugRepository'
+import DrugTypeRepository from '../../../domain/drug_type/DrugTypeRepository'
+import NotFoundError from '../../errors/not_found'
+
+export const makeAddDrugTypeToDrug = (
+	drugRepository: DrugRepository,
+	drugTypeRepository: DrugTypeRepository
+) => {
+	return async (drugName: string, dtype: string): Promise<Drug> => {
+		const drugType = await drugTypeRepository.findByType(dtype)
+
+		if (drugType === undefined) {
+			throw new NotFoundError(
+				`El tipo de fármaco '${dtype}' no está registrado.`
+			)
+		}
+
+		const drug = await drugRepository.findByName(drugName)
+
+		if (drug === undefined) {
+			throw new NotFoundError(`El fármaco '${drugName}' no está registrado.`)
+		}
+
+		drug.getDrugTypes().push(drugType)
+		await drugRepository.update(drugName, drug)
+
+		return drug
+	}
+}
