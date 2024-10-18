ff74267c6dd90e9a047064e10f83b5f4ebd2a600
AnibalGonzalezV <165087800+AnibalGonzalezV@users.noreply.github.com>
2024-10-18 19:13:34 -0300

    Implementación de casos de uso para Agregar Profesor y Agregar Alumno

---
+++ src/application/users/use_cases/makeAddTeacher.ts
@@ -0,0 +1,27 @@
+import Teacher from "../../../domain/teacher/Teacher";
+import TeacherRepository from "../../../domain/teacher/TeacherRepository";
+import AlreadyExistsError from "../../errors/already_exists";
+
+export const makeAddTeacher = (
+    teacherRepository: TeacherRepository
+) => {
+
+    return async (
+        name: string,
+        email: string
+    ): Promise <Teacher> => {
+
+        const existingTeacher = await teacherRepository.findByEmail(email)
+
+        if(existingTeacher != null){
+            throw new AlreadyExistsError(`El profesor con correo '${email}' ya está registrado.`)
+        }
+
+        const newTeacher = new Teacher (name, email)
+
+        await teacherRepository.add(newTeacher)
+
+        return newTeacher
+
+    }
+}
