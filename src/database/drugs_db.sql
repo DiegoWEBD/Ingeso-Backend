6e4730181df1ec0a542433eb69fe46cd8ef05d06
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-23 20:30:57 -0300

    base de datos implementada

---
+++ src/database/drugs_db.sql
@@ -0,0 +1,41 @@
+create table if not exists app_user(
+    institutional_email text primary key,
+    name text not null
+);
+
+create table if not exists student(
+    institutional_email text primary key references app_user(institutional_email)
+);
+
+create table if not exists teacher(
+    institutional_email text primary key references app_user(institutional_email)
+);
+
+create table if not exists drug_classification(
+    classification text primary key,
+    description text not null
+);
+
+create table if not exists drug(
+    name text primary key,
+    presentation text not null,
+    description text not null
+);
+
+create table if not exists drug_classification_association(
+    classification text not null references drug_classification(classification),
+    drug_name text not null references drug(name),
+    primary key (classification, drug_name)
+);
+
+create table if not exists administration_procedure(
+    drug_name text not null references drug(name),
+    method text not null,
+    procedure text not null,
+    primary key (drug_name, method)
+);
+
+create table if not exists ram(
+    drug_name text not null references drug(name),
+    reaction text not null
+);
