6047eec1e3ccef7ead98276e7d9091a7f5005b81
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-24 14:44:26 -0300

    base de datos actualizada para eliminar automaticamente filas innecesarias

--- src/database/drugs_db.sql
+++ src/database/drugs_db.sql
@@ -5,10 +5,12 @@ create table if not exists app_user(
 
 create table if not exists student(
     institutional_email text primary key references app_user(institutional_email)
+    on delete cascade
 );
 
 create table if not exists teacher(
     institutional_email text primary key references app_user(institutional_email)
+    on delete cascade
 );
 
 create table if not exists drug_classification(
@@ -23,19 +25,23 @@ create table if not exists drug(
 );
 
 create table if not exists drug_classification_association(
-    classification text not null references drug_classification(classification),
-    drug_name text not null references drug(name),
+    classification text not null references drug_classification(classification)
+    on delete cascade,
+    drug_name text not null references drug(name)
+    on delete cascade,
     primary key (classification, drug_name)
 );
 
 create table if not exists administration_procedure(
-    drug_name text not null references drug(name),
+    drug_name text not null references drug(name)
+    on delete cascade,
     method text not null,
     procedure text not null,
     primary key (drug_name, method)
 );
 
 create table if not exists ram(
-    drug_name text not null references drug(name),
+    drug_name text not null references drug(name)
+    on delete cascade,
     reaction text not null
 );
