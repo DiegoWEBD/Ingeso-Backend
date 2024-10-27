ac557212533d646080a1ab9deba4262fbf8ce6bf
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-26 22:23:55 -0300

    arreglo bugs

--- src/application/user/use_cases/find_user.ts
+++ src/application/user/use_cases/find_user.ts
@@ -1,6 +1,5 @@
 import User from '../../../domain/user/User'
 import UserRepository from '../../../domain/user/UserRepository'
-import NotFoundError from '../../errors/not_found'
 
 export const makeFindUser = (userRepository: UserRepository) => {
 	return async (institutionalEmail: string): Promise<User | null> => {
