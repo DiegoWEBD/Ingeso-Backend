fee264e2c2e814cbadff8b0f4da08292830ee2d0
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-27 09:58:21 -0300

    cambio de coneccion a base de datos

--- src/presentation/database/Database.ts
+++ src/presentation/database/Database.ts
@@ -3,20 +3,26 @@ import { IClient } from 'pg-promise/typescript/pg-subset'
 import DatabaseConnectionData from './connection'
 
 export default class Database {
-	private connectionData: DatabaseConnectionData
+	//private connectionData: DatabaseConnectionData
 	private pgp: IMain<{}, IClient>
 	private db: IDatabase<{}, IClient>
 
 	constructor() {
-		this.connectionData = {
+		/*this.connectionData = {
 			host: process.env.DB_HOST,
 			port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
 			database: process.env.DB_NAME,
 			user: process.env.DB_USER,
 			password: process.env.DB_PASSWORD,
-		}
+		}*/
+		const connectionUrl = process.env.POSTGRES_URL as string
 		this.pgp = pgPromise()
-		this.db = this.pgp(this.connectionData)
+		try {
+			this.db = this.pgp(connectionUrl)
+		} catch (error) {
+			console.log('Database connection refused.')
+			throw new Error('Database could not connect.')
+		}
 	}
 
 	async execute(query: string, params: Array<string>): Promise<void> {
