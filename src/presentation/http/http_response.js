623968d7b3ad5ef79933ffef2cbc492a6712605b
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-27 13:53:05 -0300

    borrando archivos js

--- src/presentation/http/http_response.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeHttpResponse = void 0;
-var makeHttpResponse = function (code, data) {
-    if (data === void 0) { data = null; }
-    return Object.freeze({ code: code, data: data });
-};
-exports.makeHttpResponse = makeHttpResponse;
