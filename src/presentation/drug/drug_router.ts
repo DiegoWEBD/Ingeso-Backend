c68fb6fcdbd93170d72a322eb6b73f0e9b9f21fb
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-11-09 19:55:27 -0300

    ruta delete implementada

--- src/presentation/drug/drug_router.ts
+++ src/presentation/drug/drug_router.ts
@@ -3,15 +3,17 @@ import IDrugServices from '../../application/drug/IDrugServices'
 import { makeController } from '../http/controller'
 import RequestHandler from '../http/request_handler'
 import { makeDrugRequestHandler } from './drug_request_handler'
+import { teacherAuthorizationMiddleware } from '../authorization/teacher_authorization_middleware'
 
 export const makeDrugRouter = (drugServices: IDrugServices): Router => {
 	const requestHandler: RequestHandler = makeDrugRequestHandler(drugServices)
 	const drugController = makeController(requestHandler)
 	const router = Router()
 
-	router.all('/', drugController)
-	router.post('/:name', drugController)
+	router.get('/', drugController)
 	router.get('/:name', drugController)
+	router.post('/:name', teacherAuthorizationMiddleware, drugController)
+	router.delete('/:name', teacherAuthorizationMiddleware, drugController)
 
 	return router
 }
